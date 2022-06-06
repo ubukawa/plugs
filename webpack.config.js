f0b573d3059a1b8509282a543d2cd1fca13b9a2e
ubukawa <ubukawa-t96dq@mlit.go.jp>
2022-06-06 00:58:55 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,14 @@ const path = require('path'); //path module
 
 module.exports = {
     mode: 'development', //use production at the end
-    entry: './src/index.js',
+    entry: './src/index.js', //edit this 
+    //entry: './src/index01.js',
+    //entry: './src/index02.js',
+    //entry: './src/index03.js',
     output: {
         path: path.resolve(__dirname, 'docs'),
-		filename: 'bundle-01.js' //
+		filename: 'bundle-01.js' // edit this
+        //filename: 'bundle-02.js'
 	},
     module:{ // モジュールプロパティの中に
         rules:[
